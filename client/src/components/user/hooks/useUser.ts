adb11d4cac6bb980030fafef9f5baee1ec1f7a07
Sasha Pimenov <pimenovs33@gmail.com>
2024-09-14 20:28:24 +0300

    Added useQuery to useUser authentication

--- client/src/components/user/hooks/useUser.ts
+++ client/src/components/user/hooks/useUser.ts
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 
 import type { User } from "@shared/types";
@@ -5,32 +6,41 @@ import type { User } from "@shared/types";
 import { useLoginData } from "@/auth/AuthContext";
 import { axiosInstance, getJWTHeader } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
+import { generateUserKey } from "@/react-query/key-factories";
 
 // query function
-// async function getUser(userId: number, userToken: string) {
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${userId}`,
-//     {
-//       headers: getJWTHeader(userToken),
-//     }
-//   );
+async function getUser(userId: number, userToken: string) {
+	const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+		`/user/${userId}`,
+		{
+			headers: getJWTHeader(userToken),
+		}
+	);
 
-//   return data.user;
-// }
+	return data.user;
+}
 
 export function useUser() {
-  // TODO: call useQuery to update user data from server
-  const user: User = null;
+	// get Details on the userId
+	const { userId, userToken } = useLoginData();
 
-  // meant to be called from useAuth
-  function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
-  }
+	// call useQuery to update user data from server
+	const { data: user } = useQuery({
+		enabled: !!userId,
+		queryKey: generateUserKey(userId, userToken),
+		queryFn: () => getUser(userId, userToken),
+		staleTime: Infinity,
+	});
 
-  // meant to be called from useAuth
-  function clearUser() {
-    // TODO: reset user to null in query cache
-  }
+	// meant to be called from useAuthActions
+	function updateUser(newUser: User): void {
+		// TODO: update the user in the query cache
+	}
 
-  return { user, updateUser, clearUser };
+	// meant to be called from useAuthActions
+	function clearUser() {
+		// TODO: reset user to null in query cache
+	}
+
+	return { user, updateUser, clearUser };
 }
